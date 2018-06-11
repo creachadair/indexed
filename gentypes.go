e56d7352c87d34805b8539df36b68cd90c4c2544
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-10 21:56:55 -0700

    Rename cmd to mktype, so a blind install won't do silly things.
    Also update the gentypes.go file.

--- gentypes.go
+++ gentypes.go
@@ -4,5 +4,5 @@ package filter
 // Filterable interface. To add additional wrappers, append them to this list
 // and re-run "go generate".
 
-//go:generate go run cmd/mktype.go -type stringFilter -base string -cons Strings -out types.go -pkg filter
-//go:generate go run cmd/mktype.go -type intFilter -base int -cons Ints -out types.go -append
+//go:generate go run mktype/mktype.go -type stringFilter -base string -cons Strings -out types.go -pkg filter
+//go:generate go run mktype/mktype.go -type intFilter -base int -cons Ints -out types.go -append
